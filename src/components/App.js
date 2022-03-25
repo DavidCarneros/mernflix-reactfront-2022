2d7cb2c925d1842162fc7057fb8fe7c048c776f2
David Carneros <David.Carneros1@alu.uclm.es>
2022-03-25 17:55:41 +0100

    Deployment

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import MyMovieList from './movies/MyMovieList';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Routes>
           <Route path="/" element={<Login />} />
