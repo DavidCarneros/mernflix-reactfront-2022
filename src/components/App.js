2bfc05cc2b81a19272e9e1e9c5606a89c9833f50
David Carneros <David.Carneros1@alu.uclm.es>
2022-03-25 17:17:36 +0100

    Added MyMovieList

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './Login';
 import MovieList from './movies/MovieList';
 import ShowMovie from './movies/ShowMovie';
+import MyMovieList from './movies/MyMovieList';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<MovieList/>} />
           <Route path="/home/details/:id" element={<ShowMovie/>} />
+          <Route path="/home/bookmarks" element={<MyMovieList/>} />
         </Routes>
       </div>
     </Router>
--- src/utils/apicalls.js
+++ src/utils/apicalls.js
@@ -1,7 +1,7 @@
 import API from './api';
 
 export  {
-    getAllMovies, addNewBookmark, getSingleMovie
+    getAllMovies, addNewBookmark, getSingleMovie, getMyBookmarks, deleteBookmark
 }
 
 function getAllMovies() {
@@ -18,3 +18,11 @@ function addNewBookmark(email, movie) {
 function getSingleMovie(idmovie) {
     return API.get('/movies/'+idmovie).then(res => res.data);
  }
+
+ function getMyBookmarks(email) {
+    return API.get('/bookmarks/'+email).then(res => res.data);
+}
+
+function deleteBookmark(idbookmark) {
+    return API.delete('/bookmarks/'+idbookmark).then(result => result.data);
+}
